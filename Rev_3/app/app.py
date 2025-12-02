1f2f9b5c3dc21a95129222140ed94534ed93b1f8
Jules Musquin <julesmusquin@protonmail.com>
2025-12-02 21:41:39 +0100

    Rev_3

---
+++ Rev_3/app/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import render_template
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    error = ""
+    result = ""
+    data_input = ""
+    list_key = []
+    if request.methods == "POST":
+        try:
+            data_input = request.form["input"]
+            result = json.loads(data_input)[0]
+            list_key = list(result.keys())
+        except json.JSONDecodeError as e:
+            error = f"Invalid Json input"
+        except Exception as e:
+            error = f"Unexpected Error"
+    return render_template(
+        "templates/index.html",
+        error=error,
+        data=data_input,
+        result=result,
+        key=list_key,
+    )
---
+++ Rev_3/readme.md
@@ -0,0 +1,5 @@
+J'ai essayé d'appliquer ce tutoriel
+
+https://medium.com/@junpyoo50/transforming-json-input-into-html-table-view-with-flask-and-jinja-a-step-by-step-guide-1d62e2fa49ed
+
+
